07a7f806540a93052bd2467879b3254c15009ae1
Oleksander Riznychuk <alexrospersonal@gmail.com>
2025-05-08 18:10:50 +0300

    Add confirm prompt with saving changes to file

--- src/commands/review.rs
+++ src/commands/review.rs
@@ -1,22 +1,36 @@
 use std::error::Error;
 use std::path::PathBuf;
 use colored::{Color, Colorize};
+use dialoguer::Confirm;
 use similar::{ChangeTag, TextDiff};
 
 pub async fn execute(path_buf: PathBuf) -> Result<(), Box<dyn Error>> {
-    let original_file = tokio::fs::read_to_string(path_buf).await?;
+    let original_file = tokio::fs::read_to_string(&path_buf).await?;
     let suggestion = mock_ai_suggestion(&original_file);
 
-    print_changing_to_cli(&original_file, &suggestion);
+    print_diff_to_cli(&original_file, &suggestion);
     
     let review_name = format!("review_{}.diff", chrono::Utc::now().timestamp());
     let diff_as_string = create_diff_string(&original_file, &suggestion);
-    save_file_to_tmp_folder(review_name, diff_as_string).await?;
+
+    let apply = Confirm::new()
+        .with_prompt(format!("Apply changes to {}?", &path_buf.display()))
+        .default(false)
+        .interact()?;
+
+    if apply {
+        tokio::fs::write(&path_buf, suggestion).await?;
+        save_diff_to_temp_file(review_name, diff_as_string).await?;
+        println!("✅ Changes applied to: {}", path_buf.display());
+    } else {
+        println!("❌ Changes were not applied.");
+    }
+    
 
     Ok(())
 }
 
-fn print_changing_to_cli(old: &str, new: &str) {
+fn print_diff_to_cli(old: &str, new: &str) {
     let diff = TextDiff::from_lines(old, new);
     
     for change in diff.iter_all_changes(){
@@ -52,7 +66,7 @@ fn create_diff_string(old: &str, new: &str) -> String {
     diff_as_string
 }
 
-async fn save_file_to_tmp_folder(review_name: String, file: String) -> Result<(), Box<dyn Error>> {
+async fn save_diff_to_temp_file(review_name: String, file: String) -> Result<(), Box<dyn Error>> {
     let temp_path = std::env::temp_dir().join(review_name);
     
     tokio::fs::write(&temp_path, file).await?;
@@ -62,11 +76,9 @@ async fn save_file_to_tmp_folder(review_name: String, file: String) -> Result<()
 }
 
 fn mock_ai_suggestion(file: &str) -> String {
-   let new_file: String = file.lines().map(|row| {
-       let mut row = row.to_string();
-       row.push_str("// suggested \n");
-       row
-   }).collect();
-
-    new_file
+    file
+        .lines()
+        .map(|row| format!("{row} // suggested"))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
